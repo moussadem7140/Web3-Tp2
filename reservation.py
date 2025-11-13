c9e4c4ba0ff2d56bef3a1c60c469027ba68396fa
Valdess-Pombouh-Tasse <valdesstasse@gmail.com>
2025-11-13 07:17:33 -0500

    Televersement d'image et suppression de service

--- reservation.py
+++ reservation.py
@@ -1,7 +1,13 @@
 """Gestion du bluprint de compte utilisateur"""
 import re
-from flask import Blueprint, request, render_template, redirect, flash, session, url_for
+from flask import Blueprint, request, render_template, redirect, flash, session, url_for, abort
 import utils
 import bd
 
 bp_reservation = Blueprint('reservation', __name__)
+
+@bp_reservation.route('/reservation', methods=['GET'])
+def reservation():
+    """deconnecter"""
+    return render_template("reservation/reservation.jinja")
+
